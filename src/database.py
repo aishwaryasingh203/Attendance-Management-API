c7ad93430185f8790a923901749853b70bb47b6f
aishwarya singh <aishwaryasingh28072003@gmail.com>
2026-04-26 00:47:27 +0530

    Fix database connection for production

--- src/database.py
+++ src/database.py
@@ -1,11 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
-DATABASE_URL = "mysql+pymysql://root:password@localhost:3306/skillbridge_db"
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:password@localhost/your_db_name")
 
 engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
